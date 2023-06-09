df475012ffe0a6c1921d320deb9c1bd93e60ae35
Ali Afarinifard <aliafarinifard@outlook.com>
2023-06-09 17:19:55 +0330

    Shoe Store Webiste use html-css-js

---
+++ JS/script.js
@@ -0,0 +1,59 @@
+// Menu Bar
+let menu = document.querySelector('#menu-icon');
+let navMenu = document.querySelector('.nav-menu');
+
+menu.onclick = () => {
+    menu.classList.toggle('fa-xmark');
+    navMenu.classList.toggle('active')
+};
+// Menu Bar
+
+
+
+// Window Scrolling (Hide NavMenu)
+window.onscroll = () => {
+    
+};
+// Window Scrolling (Hide NavMenu)
+
+
+
+// Scrolling (Move Header & Hide NavMenu)
+window.onscroll = () => {
+
+    // Move Header
+    if(window.scrollY > 0) {
+        document.querySelector('.navbar').classList.add('active');
+    } else {
+        document.querySelector('.navbar').classList.remove('active');
+    }
+
+    // Hide NavMenu
+    menu.classList.remove('fa-xmark');
+    navMenu.classList.remove('active');
+
+};
+// Scrolling (Move Header & Hide NavMenu)
+
+
+
+// OnLoad
+window.onload = () => {
+
+    if (window.scrollY > 0) {
+        document.querySelector('.navbar').classList.add('active');
+    } else {
+        document.querySelector('.navbar').classList.remove('active');
+    }
+
+};
+// OnLoad
+
+
+
+// About (Set Image)
+function functio (small) {
+    const full = document.getElementById('image-box');
+    full.src = small.src;
+}
+// About (Set Image)
